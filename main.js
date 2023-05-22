3675de915d79d3906e696d6ec657fa4314287f6d
Chitrakar002 <130827258+Chitrakar002@users.noreply.github.com>
2023-05-22 14:32:44 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,20 +3,39 @@ const audio = document.querySelector('#audio');
 const infinity = document.querySelector('#infinity');
 const heart = document.querySelector('#heart');
 const mute = document.querySelector('#volume');
+const mute_btn = document.querySelector('#volume_btn');
+const heart_btn = document.querySelector('#heart_btn');
 // console.log(audio);
 // console.log(infinity);
 // console.log(heart);
-mute.addEventListener('click',()=>{
+// console.dir(audio);
+mute_btn.addEventListener('click',()=>{
     mute.classList.toggle('fa-volume-high');
     mute.classList.toggle('fa-volume-xmark');
+    if(mute.classList.contains('fa-volume-high'))
+    {
+        audio.muted = false;
+    }
+    else if(mute.classList.contains('fa-volume-xmark'))
+    {
+        audio.muted = true;
+    }
+   
 })
-heart.addEventListener('click',()=>{
+heart_btn.addEventListener('click',()=>{
     heart.classList.toggle('fa-regular');
     heart.classList.toggle('fa-solid');
 
 })
 infinity.addEventListener('click',()=>{
     infinity.classList.toggle('loop');
+    if(infinity.classList.contains('loop'))
+    {
+        audio.loop = true;
+    }
+    else{
+        audio.loop = false;
+    }
 })
 
 play.addEventListener('click',()=>{
